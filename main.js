2f84f3fa9f135303af4f101dd32cd046b735b5f9
Lucas Tadaieski <dev.lucastadaieski@gmail.com>
2025-04-02 22:28:07 -0300

    aprendendo funcoes anonimas e texto dinamico

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-document.querySelector('.tecla_pom');
+function tocaSom (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1
+    
+    //console.log(idAudio);
+    //console.log(contador);
+}
